7c6317df0b43ccf89ea063be1f1add634f5b481a
MuhammadUsman-EE <68194326+MuhammadUsman-EE@users.noreply.github.com>
2022-05-23 15:59:50 +0200

    SG logo

--- lib/Screens/Sensor Menu/components/background.dart
+++ lib/Screens/Sensor Menu/components/background.dart
@@ -18,19 +18,11 @@ class Background extends StatelessWidget {
         children: <Widget>[
           Positioned(
             top: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/images/main_top.png",
-              width: size.width * 0.35,
-            ),
-          ),
-          Positioned(
-            bottom: 0,
             right: 0,
             child: Image.asset(
-              "assets/images/login_bottom.png",
-              width: size.width * 0.4,
+              "assets/icons/logo.png",
             ),
+            width: size.width * 0.3,
           ),
           child,
         ],
