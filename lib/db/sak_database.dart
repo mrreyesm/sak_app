fe857a7e4843838d54ca6877b71061810fd0d2b4
Marco <ma_reyesm@outlook.com>
2022-06-13 20:01:50 +0200

    database version 2

--- lib/db/sak_database.dart
+++ lib/db/sak_database.dart
@@ -19,7 +19,7 @@ class SakDatabase {
   Future<Database> _initDB(String filePath) async {
     final dbPath = await getDatabasesPath();
     final path = join(dbPath, filePath);
-    return await openDatabase(path, version: 1, onCreate: _createDB);
+    return await openDatabase(path, version: 2, onCreate: _createDB);
   }
 
   Future<void> _createDB(Database db, int version) async {
@@ -38,19 +38,39 @@ ${UserFields.employeeId} $textType,
 ${UserFields.phoneNumber} $textType,
 ${UserFields.password} $textType
 )
-''');        
+'''); 
+
+    await db.execute('''
+CREATE TABLE $tableAcceleromtr(
+${AcceleromtrFields.id} $idType,
+${AcceleromtrFields.sensor} $textType,
+${AcceleromtrFields.name} $textType,
+${AcceleromtrFields.xAxis} $textType,
+${AcceleromtrFields.yAxis} $textType,
+${AcceleromtrFields.zAxis} $textType,
+${AcceleromtrFields.time} $textType,
+)
+'''); 
+
+    await db.execute('''
+CREATE TABLE $tableGyro(
+${GyroFields.id} $idType,
+${GyroFields.sensor} $textType,
+${GyroFields.name} $textType,
+${GyroFields.xAxis} $textType,
+${GyroFields.yAxis} $textType,
+${GyroFields.zAxis} $textType,
+${GyroFields.time} $textType,
+)
+''');
   }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ///Users
+  ///////////////////////////////////////////////////////////////////////////////
 
   Future<User> create(User user) async {
     final db = await instance.database;
-
-        // final json = user.toJson();
-    // final columns =
-    //     '${UserFields.title}, ${UserFields.description}, ${UserFields.time}';
-    // final values =
-    //     '${json[UserFields.title]}, ${json[UserFields.description]}, ${json[UserFields.time]}';
-    // final id = await db
-    //     .rawInsert('INSERT INTO table_name ($columns) VALUES ($values)');
     final id = await db.insert(tableUsers, user.toJson());
     return user.copy(id: id);
   }
@@ -75,8 +95,6 @@ ${UserFields.password} $textType
   Future<List<User>> readAllUsers() async {
     final db = await instance.database;
     final orderBy = '${UserFields.id} DESC';   
-    // final result =
-    //     await db.rawQuery('SELECT * FROM $tableUsers ORDER BY $orderBy');
     final result = await db.query(tableUsers, orderBy: orderBy);
 
     return result.map((json) => User.fromJson(json)).toList();
@@ -104,6 +122,129 @@ ${UserFields.password} $textType
   }
 
   Future<void> close() async {
+    final db = await instance.database;
+    db.close();
+  }
+
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ///Acceleromtr
+  ///////////////////////////////////////////////////////////////////////////////
+  
+    Future<Acceleromtr> createAcceleromtr(Acceleromtr acceleromtr) async {
+    final db = await instance.database;
+    final id = await db.insert(tableAcceleromtr, acceleromtr.toJson());
+    return acceleromtr.copy(id: id);
+  }
+
+  Future<Acceleromtr> readAcceleromtr(int id) async {
+    final db = await instance.database;
+
+    final maps = await db.query(
+      tableAcceleromtr,
+      columns: AcceleromtrFields.values,
+      where: '${AcceleromtrFields.id} = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return Acceleromtr.fromJson(maps.first);
+    } else {
+      throw Exception('ID $id not found');
+    }
+  }
+
+  Future<List<Acceleromtr>> readAllAcceleromtrs() async {
+    final db = await instance.database;
+    final orderBy = '${AcceleromtrFields.id} DESC';   
+    final result = await db.query(tableAcceleromtr, orderBy: orderBy);
+
+    return result.map((json) => Acceleromtr.fromJson(json)).toList();
+  }
+
+  Future<void> updateAcceleromtr(Acceleromtr acceleromtr) async {
+    final db = await instance.database;
+
+    await db.update(
+      tableAcceleromtr,
+      acceleromtr.toJson(),
+      where: '${AcceleromtrFields.id} = ?',
+      whereArgs: [acceleromtr.id],
+    );
+  }
+
+  Future<void> deleteAcceleromtr(int id) async {
+    final db = await instance.database;
+
+    await db.delete(
+      tableAcceleromtr,
+      where: '${AcceleromtrFields.id} = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<void> closeAcceleromtr() async {
+    final db = await instance.database;
+    db.close();
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ///Gyroscope
+  ////////////////////////////////////////////////////////////////////////////////
+  
+    Future<Gyro> createGyro(Gyro gyro) async {
+    final db = await instance.database;
+    final id = await db.insert(tableGyro, gyro.toJson());
+    return gyro.copy(id: id);
+  }
+
+  Future<Gyro> readGyro(int id) async {
+    final db = await instance.database;
+
+    final maps = await db.query(
+      tableGyro,
+      columns: GyroFields.values,
+      where: '${GyroFields.id} = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return Gyro.fromJson(maps.first);
+    } else {
+      throw Exception('ID $id not found');
+    }
+  }
+
+  Future<List<Gyro>> readAllGyros() async {
+    final db = await instance.database;
+    final orderBy = '${GyroFields.id} DESC';   
+    final result = await db.query(tableGyro, orderBy: orderBy);
+
+    return result.map((json) => Gyro.fromJson(json)).toList();
+  }
+
+  Future<void> updateGyro(Gyro gyro) async {
+    final db = await instance.database;
+
+    await db.update(
+      tableGyro,
+      gyro.toJson(),
+      where: '${GyroFields.id} = ?',
+      whereArgs: [gyro.id],
+    );
+  }
+
+  Future<void> deleteGyro(int id) async {
+    final db = await instance.database;
+
+    await db.delete(
+      tableGyro,
+      where: '${GyroFields.id} = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<void> closeGyro() async {
     final db = await instance.database;
     db.close();
   }
--- lib/model/sak.dart
+++ lib/model/sak.dart
@@ -1,5 +1,7 @@
 
 final String tableUsers = 'users';
+final String tableAcceleromtr = 'acceleromtr';
+final String tableGyro = 'gyro';
 
 class UserFields {
   static final List<String> values = [
@@ -19,6 +21,46 @@ class UserFields {
   static final String employeeId = 'employeeId';
   static final String phoneNumber = 'phoneNumber';
   static final String password = 'password';
+}
+
+class AcceleromtrFields {
+  static final List<String> values = [
+    id,
+    sensor,
+    name,
+    xAxis,
+    yAxis,
+    zAxis,
+    time
+  ];
+
+  static final String id = '_id';
+  static final String sensor = 'sensor';
+  static final String name = 'name';
+  static final String xAxis = 'xAxis';
+  static final String yAxis = 'yAxis';
+  static final String zAxis = 'zAxis';
+  static final String time = 'time';
+}
+
+class GyroFields {
+  static final List<String> values = [
+    id,
+    sensor,
+    name,
+    xAxis,
+    yAxis,
+    zAxis,
+    time
+  ];
+
+  static final String id = '_id';
+  static final String sensor = 'sensor';
+  static final String name = 'name';
+  static final String xAxis = 'xAxis';
+  static final String yAxis = 'yAxis';
+  static final String zAxis = 'zAxis';
+  static final String time = 'time';
 }
 
 class User {
@@ -80,6 +122,134 @@ class User {
   UserFields.employeeId: employeeId,
   UserFields.phoneNumber: phoneNumber,
   UserFields.password: password,
+};
+
+}
+
+
+class Acceleromtr {
+  final int? id;
+  final String sensor;
+  final String name;
+  final String xAxis;
+  final String yAxis;
+  final String zAxis;
+  final String time;
+  
+  const Acceleromtr({
+    this.id,
+    required this.sensor,
+    required this.name,
+    required this.xAxis,
+    required this.yAxis,
+    required this.zAxis,
+    required this.time,
+  }); 
+  
+  Acceleromtr copy({
+    int? id,
+    String? sensor,
+    String? name,
+    String? xAxis,
+    String? yAxis,
+    String? zAxis,
+    String? time,
+  }) {
+    return Acceleromtr(
+      id: id ?? this.id,
+      sensor: sensor ?? this.sensor,
+      name: name ?? this.name,
+      xAxis: xAxis ?? this.xAxis,
+      yAxis: yAxis ?? this.yAxis,
+      zAxis: zAxis ?? this.zAxis,
+      time: time ?? this.time,
+    );
+  }
+
+  static Acceleromtr fromJson(Map<String, dynamic> json) {
+    return Acceleromtr(
+      id: json[AcceleromtrFields.id] as int,
+      sensor: json[AcceleromtrFields.sensor] as String,
+      name: json[AcceleromtrFields.name] as String,
+      xAxis: json[AcceleromtrFields.xAxis] as String,
+      yAxis: json[AcceleromtrFields.yAxis] as String,
+      zAxis: json[AcceleromtrFields.zAxis] as String,
+      time: json[AcceleromtrFields.time] as String,
+    );
+  }
+
+  Map<String, Object?> toJson() => {
+  AcceleromtrFields.id: id,
+  AcceleromtrFields.sensor: sensor,
+  AcceleromtrFields.name: name,
+  AcceleromtrFields.xAxis: xAxis,
+  AcceleromtrFields.yAxis: yAxis,
+  AcceleromtrFields.zAxis: zAxis,
+  AcceleromtrFields.time: time,  
+};
+
+}
+
+
+class Gyro {
+  final int? id;
+  final String sensor;
+  final String name;
+  final String xAxis;
+  final String yAxis;
+  final String zAxis;
+  final String time;
+  
+  const Gyro({
+    this.id,
+    required this.sensor,
+    required this.name,
+    required this.xAxis,
+    required this.yAxis,
+    required this.zAxis,
+    required this.time,
+  }); 
+  
+  Gyro copy({
+    int? id,
+    String? sensor,
+    String? name,
+    String? xAxis,
+    String? yAxis,
+    String? zAxis,
+    String? time,
+  }) {
+    return Gyro(
+      id: id ?? this.id,
+      sensor: sensor ?? this.sensor,
+      name: name ?? this.name,
+      xAxis: xAxis ?? this.xAxis,
+      yAxis: yAxis ?? this.yAxis,
+      zAxis: zAxis ?? this.zAxis,
+      time: time ?? this.time,
+    );
+  }
+
+  static Gyro fromJson(Map<String, dynamic> json) {
+    return Gyro(
+      id: json[GyroFields.id] as int,
+      sensor: json[GyroFields.sensor] as String,
+      name: json[GyroFields.name] as String,
+      xAxis: json[GyroFields.xAxis] as String,
+      yAxis: json[GyroFields.yAxis] as String,
+      zAxis: json[GyroFields.zAxis] as String,
+      time: json[GyroFields.time] as String,
+    );
+  }
+
+  Map<String, Object?> toJson() => {
+  GyroFields.id: id,
+  GyroFields.sensor: sensor,
+  GyroFields.name: name,
+  GyroFields.xAxis: xAxis,
+  GyroFields.yAxis: yAxis,
+  GyroFields.zAxis: zAxis,
+  GyroFields.time: time,  
 };
 
 }
