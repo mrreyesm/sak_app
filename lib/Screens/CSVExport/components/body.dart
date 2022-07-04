90996b2702512092249e8b9e8190ea2ca63db2f3
Marco <ma_reyesm@outlook.com>
2022-07-04 15:03:03 +0200

    working download csv

--- lib/Screens/CSVExport/components/body.dart
+++ lib/Screens/CSVExport/components/body.dart
@@ -5,6 +5,10 @@ import 'package:sak_app/Screens/Sensor%20Menu/sensorMenu.dart';
 import 'package:sak_app/db/sak_database.dart';
 import 'package:sak_app/model/sak.dart';
 import 'package:intl/intl.dart';
+import 'package:csv/csv.dart';
+import 'package:external_path/external_path.dart';
+import 'dart:io';
+
 
 class SensorsPage extends StatefulWidget {
   @override
@@ -13,6 +17,7 @@ class SensorsPage extends StatefulWidget {
 
 class _SensorsPageState extends State<SensorsPage> {
   late List<Sensor> sensors = sensors;
+  late List<Sensor> sensorcsv = sensorcsv;
   bool isLoading = false;
   AssetImage _downloadIcon = AssetImage("assets/icons/dlicon.png");
 
@@ -36,6 +41,69 @@ class _SensorsPageState extends State<SensorsPage> {
     this.sensors = await SakDatabase.instance.readAllSensors();
 
     setState(() => isLoading = false);
+  }
+
+  Future _downloadSensor(String name) async {
+        return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Download Sensor Data'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('Recording will be downloaded to your device.'),
+                //Text('Data stored as a CSV file.'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Okay'),
+              onPressed: () async {
+                setState(() => isLoading = true);
+                this.sensorcsv = await SakDatabase.instance.downloadSensor(name);
+                //Map<Permission, PermissionStatus> statuses = await [Permission.storage,].request();
+                List<dynamic> dynamicValues = List<dynamic>.from(this.sensorcsv);
+                print(dynamicValues);
+                List<List<dynamic>> rows = [];
+
+                List<dynamic> row = [];
+                row.add("id");
+                row.add("sensor");
+                row.add("name");
+                row.add("xAxis");
+                row.add("yAxis");
+                row.add("zAxis");
+                row.add("time");
+                rows.add(row);
+                for (int i = 0; i < dynamicValues.length; i++) {
+                  List<dynamic> row = [];
+                  row.add(dynamicValues[i].id - 1);
+                  row.add(dynamicValues[i].sensor);
+                  row.add(dynamicValues[i].name);
+                  row.add(dynamicValues[i].xAxis);
+                  row.add(dynamicValues[i].yAxis);
+                  row.add(dynamicValues[i].zAxis);
+                  row.add(dynamicValues[i].time);
+                  rows.add(row);
+                }
+                print(rows);
+                String csv = const ListToCsvConverter().convert(rows);
+                String dir = await ExternalPath.getExternalStoragePublicDirectory(ExternalPath.DIRECTORY_DOWNLOADS);
+                print("dir $dir");
+                String file = "$dir";
+                File f = File(file + "/filename2.csv");
+                f.writeAsString(csv);
+                setState(() => isLoading = false);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -79,7 +147,9 @@ class _SensorsPageState extends State<SensorsPage> {
                 image: DecorationImage(image: _downloadIcon),
               ),
             ),
-            onTap: () {},
+            onTap: () {
+              _downloadSensor(sensors[i].name);
+            },
           ),
         ))
       ]);
